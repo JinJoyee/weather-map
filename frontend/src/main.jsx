3fca41535c3b8544c69fd42eec3851dae40747a3
moonjh <mun20060304@gmail.com>
2026-05-19 23:00:25 +0900

    fix: BrowserRouter 에러 해결

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,13 @@
  import { StrictMode } from 'react';
   import { createRoot } from 'react-dom/client';
-  import { BrowerRouter } from 'react-router-dom';
+  import { BrowserRouter } from 'react-router-dom';
   import './index.css';
   import App from './App.jsx';
 
   createRoot(document.getElementById('root')).render(
     <StrictMode>
-      <BrowerRouter>
+      <BrowserRouter>
         <App />
-      </BrowerRouter>
+      </BrowserRouter>
     </StrictMode>
   );
