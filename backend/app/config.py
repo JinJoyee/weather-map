df919695653acd4db0892a4b5ecea14bb7962bc7
lxj0319 <tjwls1386@gmail.com>
2026-06-01 20:09:43 +0900

    feat: 토큰 인증 시간 제한

--- backend/app/config.py
+++ backend/app/config.py
@@ -8,3 +8,6 @@ KAKAO_REST_API_KEY = os.getenv("KAKAO_REST_API_KEY")
 JWT_SECRET = os.getenv("JWT_SECRET", "secret")
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./app.db")
 UV_API_KEY = os.getenv("UV_API_KEY")
+
+JWT_ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
